e6e553e10d55887a81ebc4c5f709ece17bcb31a2
Justin Brubaker <justinbrubaker7@gmail.com>
2021-03-06 20:26:04 -0800

    task column added and local storage working

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,4 @@
-
-// function saveData(){
-
-// }
-
+//This prevents the page from reloading when you press the enter key
 $('form').bind("keypress", function(e) {
     if (e.keyCode == 13) {               
       e.preventDefault();
@@ -11,25 +7,37 @@ $('form').bind("keypress", function(e) {
   });
 
 
-//enter text
-//click save
-//text saves to local storage
-//text remains on display
-//event prevent default
-//
+  // this submits the 9AM input when enter key is pressed
+var enterKey = document.getElementById("input-9am");
+enterKey.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   var text9am = $("#input-9am").val()
+   window.localStorage.setItem("9am", JSON.stringify(text9am));
+   var text9amReturn = JSON.parse(window.localStorage.getItem("9am"));
+   var text9am2 = $("#div-9am");
+   text9am2.value = text9amReturn;
+   document.getElementById("div-9am").innerHTML = text9amReturn;
+  }
+});
 
+
+  // this submits the 9AM input when the save button is pressed
 document.addEventListener("click", function(e){
     if(e.target && e.target.id == "save-9am"){
         e.preventDefault();
-        
         var text9am = $("#input-9am").val()
-        
         window.localStorage.setItem("9am", JSON.stringify(text9am));
         var text9amReturn = JSON.parse(window.localStorage.getItem("9am"));
-        
-        
-        var text9am2 = $("#input-9am");
+        var text9am2 = $("#div-9am");
         text9am2.value = text9amReturn;
-        console.log(text9amReturn);
+        document.getElementById("div-9am").innerHTML = text9amReturn;
     }
 });
+
+// this loads the tasks when the page is loaded
+window.onload = function(){
+  document.getElementById("div-9am").innerHTML = JSON.parse(window.localStorage.getItem("9am"));
+  //document.getElementById("input-9am").placeholder = JSON.parse(window.localStorage.getItem("9am"));
+  $("#div-9am").text = JSON.parse(window.localStorage.getItem("9am"));
+}
