be0490c573ef346f397c849d6dd34912e5b19fad
Justin Brubaker <justinbrubaker7@gmail.com>
2021-03-06 20:32:56 -0800

    10am working

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,10 @@ $('form').bind("keypress", function(e) {
     }
   });
 
-
+/*                           9                                */ 
   // this submits the 9AM input when enter key is pressed
-var enterKey = document.getElementById("input-9am");
-enterKey.addEventListener("keyup", function(event) {
+var enterKey9 = document.getElementById("input-9am");
+enterKey9.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
    event.preventDefault();
    var text9am = $("#input-9am").val()
@@ -38,6 +38,41 @@ document.addEventListener("click", function(e){
 // this loads the tasks when the page is loaded
 window.onload = function(){
   document.getElementById("div-9am").innerHTML = JSON.parse(window.localStorage.getItem("9am"));
-  //document.getElementById("input-9am").placeholder = JSON.parse(window.localStorage.getItem("9am"));
   $("#div-9am").text = JSON.parse(window.localStorage.getItem("9am"));
+
+  //10
+  document.getElementById("div-10am").innerHTML = JSON.parse(window.localStorage.getItem("10am"));
+  $("#div-10am").text = JSON.parse(window.localStorage.getItem("10am"));
 }
+
+
+/*                           10                                */ 
+
+  // this submits the 9AM input when enter key is pressed
+  var enterKey10 = document.getElementById("input-10am");
+  enterKey10.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     var text10am = $("#input-10am").val()
+     window.localStorage.setItem("10am", JSON.stringify(text10am));
+     var text10amReturn = JSON.parse(window.localStorage.getItem("10am"));
+     var text10am2 = $("#div-10am");
+     text10am2.value = text10amReturn;
+     document.getElementById("div-10am").innerHTML = text10amReturn;
+    }
+  });
+  
+  
+    // this submits the 9AM input when the save button is pressed
+  document.addEventListener("click", function(e){
+      if(e.target && e.target.id == "save-10am"){
+          e.preventDefault();
+          var text10am = $("#input-10am").val()
+          window.localStorage.setItem("10am", JSON.stringify(text10am));
+          var text10amReturn = JSON.parse(window.localStorage.getItem("10am"));
+          var text10am2 = $("#div-10am");
+          text10am2.value = text10amReturn;
+          document.getElementById("div-10am").innerHTML = text10amReturn;
+      }
+  });
+  
